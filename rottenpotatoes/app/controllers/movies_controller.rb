cb9c029edf3f5e1092f6e80a267bc36a2c9ac790
Brandon Beckwith <bbeckwi2@uncc.edu>
2016-11-17 03:51:52 +0000

    Finished with cucumber

--- rottenpotatoes/app/controllers/movies_controller.rb
+++ rottenpotatoes/app/controllers/movies_controller.rb
@@ -1,7 +1,7 @@
 class MoviesController < ApplicationController
   
   def movie_params
-    params.require(:movie).permit(:title, :rating, :description, :release_date)
+    params.require(:movie).permit(:title, :rating, :description, :release_date, :director)
   end
 
   def show
@@ -59,6 +59,19 @@ class MoviesController < ApplicationController
     @movie.destroy
     flash[:notice] = "Movie '#{@movie.title}' deleted."
     redirect_to movies_path
+  end
+  
+  def similar_movies
+    
+    @movie = Movie.find(params[:id])
+    @movies = Movie.where(director: @movie.director)
+    
+    t = false
+    
+    if @movie.director == "" then
+      flash[:notice] = "'#{@movie.title}' has no director info"
+      redirect_to movies_path
+    end
   end
 
 end
--- rottenpotatoes/config/routes.rb
+++ rottenpotatoes/config/routes.rb
@@ -1,5 +1,7 @@
 Rottenpotatoes::Application.routes.draw do
   resources :movies
   # map '/' to be a redirect to '/movies'
+  
+  get 'movies/:id/simliar_movies' => 'movies#similar_movies', :as => :similar_movies
   root :to => redirect('/movies')
 end
